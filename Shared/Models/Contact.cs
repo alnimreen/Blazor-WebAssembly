7d68d4e86197a6a2957e0e2595a0bd2ad78bdd19
lana alnimreen <98852341+alnimreen@users.noreply.github.com>
2024-04-07 00:41:02 +0300

    Add files via upload

---
+++ Shared/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactProject.Shared.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Shared/Services/ContactService.cs
@@ -0,0 +1,57 @@
+﻿using ContactProject.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactProject.Shared.Services
+{
+    public class ContactService
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public List<Contact> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contact.Id = contacts.Count + 1; 
+            contacts.Add(contact);
+        }
+
+        public async Task UpdateContact(Contact updatedContact)
+        {
+            var existingContact = contacts.FirstOrDefault(c => c.Id == updatedContact.Id);
+            if (existingContact != null)
+            {
+                // Create a copy of the updated contact
+                var updatedCopy = new Contact
+                {
+                    Id = existingContact.Id,
+                    FirstName = updatedContact.FirstName,
+                    LastName = updatedContact.LastName,
+                    Email = updatedContact.Email,
+                    PhoneNumber = updatedContact.PhoneNumber
+                };
+
+                // Find the index of the existing contact
+                var index = contacts.IndexOf(existingContact);
+                // Replace the existing contact with the updated copy
+                contacts[index] = updatedCopy;
+            }
+        }
+
+
+        public void DeleteContact(int contactId)
+        {
+            var contactToRemove = contacts.FirstOrDefault(c => c.Id == contactId);
+            if (contactToRemove != null)
+            {
+                contacts.Remove(contactToRemove);
+            }
+        }
+    }
+    }
